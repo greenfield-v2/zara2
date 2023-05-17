60858a82c9c3337a38793e7886964ddda314cc59
Tarek-Issaoui <tarek9.9issaoui@gmail.com>
2023-05-17 19:26:29 +0100

    .

--- backend/schema.sql
+++ backend/schema.sql
@@ -3,15 +3,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
 -- Schema zara
 -- -----------------------------------------------------
@@ -76,7 +67,6 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-USE `mydb` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- frontend/pages/components/Navbar.tsx
+++ frontend/pages/components/Navbar.tsx
@@ -64,4 +64,4 @@ const NavBar = () => {
             </div>
           )
 }
-  
+  export default NavBar;
