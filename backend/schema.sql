52d13bdf984b6c7eb51cf5a7b3dc20bd796321ce
Tarek-Issaoui <tarek9.9issaoui@gmail.com>
2023-05-17 20:34:59 +0100

    .

--- backend/schema.sql
+++ backend/schema.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema zara
 -- -----------------------------------------------------
 
@@ -15,14 +18,14 @@ CREATE SCHEMA IF NOT EXISTS `zara` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4
 USE `zara` ;
 
 -- -----------------------------------------------------
--- Table `zara`.`product`
+-- Table `zara`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zara`.`product` (
+CREATE TABLE IF NOT EXISTS `zara`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `clothesName` VARCHAR(45) NOT NULL,
-  `image` VARCHAR(255) NOT NULL,
-  `price` INT NOT NULL,
-  `category` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(225) NOT NULL,
+  `isAdmin` TINYINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -34,12 +37,12 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zara`.`cart` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `product_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_cart_product_idx` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cart_product`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `zara`.`product` (`id`)
+  INDEX `fk_cart_user_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_cart_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `zara`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -48,18 +51,18 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `zara`.`user`
+-- Table `zara`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zara`.`user` (
+CREATE TABLE IF NOT EXISTS `zara`.`product` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(225) NOT NULL,
-  `isAdmin` TINYINT NOT NULL DEFAULT '0',
+  `clothesName` VARCHAR(45) NOT NULL,
+  `image` VARCHAR(255) NOT NULL,
+  `price` INT NOT NULL,
+  `category` VARCHAR(45) NOT NULL,
   `cart_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_user_cart1_idx` (`cart_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_cart1`
+  INDEX `fk_product_cart1_idx` (`cart_id` ASC) VISIBLE,
+  CONSTRAINT `fk_product_cart1`
     FOREIGN KEY (`cart_id`)
     REFERENCES `zara`.`cart` (`id`)
     ON DELETE NO ACTION
@@ -67,7 +70,6 @@ CREATE TABLE IF NOT EXISTS `zara`.`user` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
-
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
