457fb51a35a86bafa8e6a662232fe934b6ac0d99
AdamSaafy <adamsaafy5@gmail.com>
2023-05-17 20:33:45 +0100

    schema last version

--- backend/schema.sql
+++ backend/schema.sql
@@ -4,70 +4,62 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema ZARA
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema zara
+-- Schema ZARA
 -- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ZARA` DEFAULT CHARACTER SET utf8 ;
+USE `ZARA` ;
 
 -- -----------------------------------------------------
--- Schema zara
+-- Table `ZARA`.`cart`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `zara` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `zara` ;
+CREATE TABLE IF NOT EXISTS `ZARA`.`cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Table `zara`.`product`
+-- Table `ZARA`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zara`.`product` (
+CREATE TABLE IF NOT EXISTS `ZARA`.`user` (
+  `iduser` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `isAdmin` TINYINT NOT NULL,
+  `cart_id` INT NOT NULL,
+  PRIMARY KEY (`iduser`, `cart_id`),
+  INDEX `fk_user_cart_idx` (`cart_id` ASC) VISIBLE,
+  CONSTRAINT `fk_user_cart`
+    FOREIGN KEY (`cart_id`)
+    REFERENCES `ZARA`.`cart` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ZARA`.`product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ZARA`.`product` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `clothesName` VARCHAR(45) NOT NULL,
   `image` VARCHAR(255) NOT NULL,
-  `price` INT NOT NULL,
   `category` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `zara`.`cart`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zara`.`cart` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `product_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_cart_product_idx` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cart_product`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `zara`.`product` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `zara`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zara`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(225) NOT NULL,
-  `isAdmin` TINYINT NOT NULL DEFAULT '0',
   `cart_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_user_cart1_idx` (`cart_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_cart1`
+  PRIMARY KEY (`id`, `cart_id`),
+  INDEX `fk_product_cart1_idx` (`cart_id` ASC) VISIBLE,
+  CONSTRAINT `fk_product_cart1`
     FOREIGN KEY (`cart_id`)
-    REFERENCES `zara`.`cart` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    REFERENCES `ZARA`.`cart` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE )
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
