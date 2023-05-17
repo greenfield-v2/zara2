0dcedee3a4dc20257bb34169d65e3d6dcd11651e
nourane-abderrahim <abderrahim.nourane@gmail.com>
2023-05-17 15:43:49 +0200

    .

--- frontend/pages/components/Navbar.tsx
+++ frontend/pages/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+//import Link from "next/link";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
