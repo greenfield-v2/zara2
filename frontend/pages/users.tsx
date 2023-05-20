aed84053b285ce0832fce843808b5bb9dac45499
Tarek-Issaoui <tarek9.9issaoui@gmail.com>
2023-05-20 20:56:33 +0100

    admin can delete any user

---
+++ frontend/pages/users.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from '../styles/users.module.css'
+const Users:React.FC=()=>{
+    const [users,setUsers]=useState<any>([])
+    const [count ,setCount]=useState<number>(0)
+    interface Customers{
+        id:number,
+        username:string,
+        email:string,
+        isAdmin:number
+    }
+    console.log(users)
+    const getAllUsers=async()=>{
+        const res=await axios.get<Customers[]>(`http://${process.env.HOST}:${process.env.PORT}/users`)
+        setUsers(res.data)
+    }
+
+    const removeUser=async(id:number)=>{
+        await axios.delete(`http://${process.env.HOST}:${process.env.PORT}/users/remove/${id}`)
+        setCount(count+1)
+    }
+    useEffect(()=>{
+        getAllUsers()
+    },[count])
+    return (<div className={styles.admin}>
+        {users.map((user,i)=>{
+        return (
+                <div key={i} className={styles.infouser}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png" alt="profile" className={styles.img}/>
+                    <div className={styles.rs}>
+                        <div className={styles.info}>
+                            <h4>{user.username}</h4>
+                            <p>{user.email}</p>
+                        </div>
+                        <button className={styles.btn} onClick={()=>removeUser(user.id)}>remove</button>   
+                    </div>
+                    <hr />
+                </div>
+        )})
+    }
+    </div> )
+            
+            
+}
+export default Users;
